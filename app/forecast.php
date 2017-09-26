da628b4893d8ad2908b889bd3d708c15110bf649
gabyya <31488533+gabyya@users.noreply.github.com>
2017-09-25 20:17:44 -0700

    changed coordinates

--- app/forecast.php
+++ app/forecast.php
@@ -1,7 +1,7 @@
 <?php
 
   // call the weather api, pass the lat_lng_array
-  $wather = 'https://api.darksky.net/forecast/'.$key.'/41.8781,-87.6298/?exclude=minutely?exclude=hourly';
+  $wather = 'https://api.darksky.net/forecast/'.$key.'/40.4168,-3.7038/?exclude=minutely?exclude=hourly';
 
   // get the data for the forcast
   $forecast = json_decode(file_get_contents($wather), true);
@@ -9,8 +9,14 @@
   //feels like
   $temp = $forecast['currently']['temperature'];
 
-  if ($temp < 50){
+  if ($temp < 30){
   	$feels = 'It\'s cold outside.';
-  } else {
-  	$feels = 'It\'s nice outside.';
+  } else if ($temp < 60) {
+  	$feels = 'You should put a sweater on (:';
+  } else if ($temp < 80) {
+  	$feels = 'It\'s nice outside. (:';
+  } else if ($temp < 100) {
+  	$feels = 'It\'s warm outside.';
+  } else if ($temp > 100) {
+  	$feels = 'My face is burning off.';
   }
